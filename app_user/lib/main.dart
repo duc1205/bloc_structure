5b0fa7d0120e9cb8d62aa8f897862467c33a370b
duc1205 <mduc5698@gmail.com>
2024-05-06 14:27:52 +0700

    fix binding

--- app_user/lib/main.dart
+++ app_user/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
   runApp(const MyApp());
 }
